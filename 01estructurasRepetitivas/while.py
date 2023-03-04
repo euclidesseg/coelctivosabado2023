136aaa09911cd3c1f4ea751839b66b1a21fc43f4
euclidesseg <euder0206@gmail.com>
2023-03-04 13:22:28 -0500

    se agrega listas y arreglos en python

---
+++ 01estructurasRepetitivas/while.py
@@ -0,0 +1,6 @@
+controladora = 1000
+
+print("")
+while(controladora !=  2):
+   controladora = int (input("Digita una opcion"))
+    
---
+++ 02listas/listas.py
@@ -0,0 +1,13 @@
+numeros = [1,2,3,4,5]
+print(numeros)
+
+print(numeros[0])
+
+#mostranto todos los elementos de una lista 
+for i in range(5,2,20):
+    print("Estoy saltando")
+
+#recorriendo una lista con python
+
+for numero in numeros:
+    print(numero)
